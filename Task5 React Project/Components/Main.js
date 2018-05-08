5ec6a6ad2d2c246c7f826f6bad05ef5a77ca9c64
Roofisonfire <pashashun123@gmail.com>
2018-05-08 14:59:34 +0400

    Messages in state

---
+++ Task5 React Project/Components/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import MainContent from './MainContent'
+
+class Main extends Component {
+render() {
+    return (
+      <div className="App-block2">
+        <MainContent/>
+      </div>
+    );
+  }
+}
+export default Main 
